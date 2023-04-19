4e8eaefd03123d0d7aa3b80db45fe319fc5f5412
jimyu <aliyah_7@naver.com>
2023-04-20 01:40:54 +0900

    style: 헤더 디자인 및 아이콘 클릭시 색상 설정

---
+++ src/components/common/Header/style.tsx
@@ -0,0 +1,36 @@
+import { Theme, css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Root = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 2px;
+
+  width: 400px;
+  padding: 10px;
+  ${({ theme }) =>
+    css`
+      background-color: ${theme.colors.white_300};
+      box-shadow: ${theme.shadow.type_2};
+    `};
+`;
+
+export const ActiveLink = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  //클릭할 때만 색 바뀌게 설정
+  &:active {
+    path {
+      stroke: ${theme.colors.primary_200};
+      color: ${theme.colors.primary_200};
+    }
+    /* 
+    img :first-child {
+      align-self: flex-start;
+    } */
+  }
+`;
