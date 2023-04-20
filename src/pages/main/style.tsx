1016969ce579231909848dfe2dd0abd476971947
jimyu <aliyah_7@naver.com>
2023-04-21 05:41:05 +0900

    style: 메인 사용자뷰 div 스타일링

--- src/pages/main/style.tsx
+++ src/pages/main/style.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Root = styled.section`
@@ -6,4 +7,25 @@ export const Root = styled.section`
   align-items: center;
 
   padding: 120px 30px 0;
+`;
+
+export const MainDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  top: -50px;
+  margin: 0;
+
+  width: 100%;
+  height: 70vh;
+  padding: 15px 20px 30px;
+
+  border-radius: 20px;
+  font-size: 20px;
+
+  ${({ theme }) =>
+    css`
+      background-color: ${theme.colors.primary_100};
+    `};
 `;
