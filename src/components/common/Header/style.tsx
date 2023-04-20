02c1c270f8ec9b6ba1a2f5a5c2f9c0ac9b8b8945
jimyu <aliyah_7@naver.com>
2023-04-21 05:25:12 +0900

    feature: 헤더 아이콘 클릭 시 색깔 바뀌게 설정

--- src/components/common/Header/style.tsx
+++ src/components/common/Header/style.tsx
@@ -16,21 +16,24 @@ export const Root = styled.header`
     `};
 `;
 
-export const ActiveLink = (theme: Theme) => css`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
+export const ActiveLink = (theme: Theme) => {
+  return css`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
 
-  //클릭할 때만 색 바뀌게 설정
-  &:active {
-    path {
-      stroke: ${theme.colors.primary_200};
-      color: ${theme.colors.primary_200};
+    //클릭할 때만 색 바뀌게 설정
+    &:active {
+      path {
+        stroke: ${theme.colors.primary_200};
+        color: ${theme.colors.primary_200};
+      }
     }
-    /* 
-    img :first-child {
+
+    /* 로고만 맨 왼쪽에 붙여주고 싶다,, */
+    /* img :first-child {
       align-self: flex-start;
-    } */
-  }
-`;
+    }; */
+  `;
+};
