3f73a290da27b0533827d6b0891193809b0258f4
seongminn <berry9901@gmail.com>
2023-04-28 00:22:35 +0900

    feat: 스크린 높이를 계산하는 함수 추가
    팝업 등으로 인해 가변적인 높이를 계산하여 컴포넌트를 랜더링합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { PATH } from './utils/path';
 
 import Loader from '@/components/common/Loader';
 
-const MainPage = lazy(() => import('./pages/main'));
+const MainPage = lazy(() => import('@/pages/main'));
+const ApplyPage = lazy(() => import('@/pages/apply'));
 
-// TODO locker, notice element 변경
+function setScreenSize() {
+  const vh = window.innerHeight * 0.01;
+
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
 function App() {
+  useEffect(() => {
+    setScreenSize();
+  }, []);
+
+  useEffect(() => {
+    const onResize = () => {
+      setScreenSize();
+    };
+
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
-      {/* // TODO LOADING 컴포넌트 생성 */}
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path={PATH.MAIN} element={<MainPage />} />
-          <Route path={PATH.LOCKER} element={<MainPage />} />
+          <Route path={PATH.LOCKER} element={<ApplyPage />}>
+            <Route path={`${PATH.LOCKER}/*`} element={<ApplyPage />} />
+          </Route>
           <Route path={PATH.NOTICE} element={<MainPage />} />
         </Routes>
       </Suspense>
