53c6dfd050234fdfef4d34b3c52d660dead8a725
seongminn <seongminn@users.noreply.github.com>
2023-04-30 12:49:05 +0000

    Deploy preview for PR 22 🛫

--- pr-preview/pr-22/src/types/emotion.d.ts
+++
@@ -1,9 +0,0 @@
-import '@emotion/react';
-import { emotion_theme } from '@/styles/theme';
-
-type CustomTheme = typeof emotion_theme;
-
-declare module '@emotion/react' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends CustomTheme {}
-}
