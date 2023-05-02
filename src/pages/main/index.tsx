ecea93fce2b1db7f392cac6c7d57ceac49de94c5
정나윤 <02imyun@hufs.ac.kr>
2023-05-03 00:34:33 +0900

    chore: 메인페이지 로그인 버튼 생성

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import * as Styled from './style';
 
 import Button from '@/components/common/Button';
 import PageTemplate from '@/components/common/PageTamplate';
+import { PATH } from '@/utils/path';
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
     <PageTemplate>
       <Styled.Root>
         <div>페이지 템플릿을 활용해봅니다.</div>
-        <Button>글씨체가 적용이 되나요?</Button>
+        <Button
+          variant='contained'
+          onClick={() => navigate(PATH.LOGIN)}
+        >
+        로그인하기
+        </Button>
       </Styled.Root>
     </PageTemplate>
   );
