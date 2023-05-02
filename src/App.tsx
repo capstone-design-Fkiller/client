3734fdd603ba85ada1f668baeb7651182630ce82
정나윤 <02imyun@hufs.ac.kr>
2023-05-02 22:02:32 +0900

    add: 로그인 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from '@/pages/login';
 
 const MainPage = lazy(() => import('@/pages/main'));
 const SelectApplyMode = lazy(() => import('@/pages/locker-mode'));
+const LoginPage = lazy(() => import('@/pages/login'));
 const ApplyPage = lazy(() => import('@/pages/locker-mode/apply'));
 
 function setScreenSize() {
@@ -40,6 +41,7 @@ function App() {
         <Route path={PATH.LOCKER} element={<SelectApplyMode />} />
         <Route path={PATH.APPLY} element={<ApplyPage />} />
         <Route path={PATH.NOTICE} element={<MainPage />} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
       </Routes>
     </Suspense>
   );
