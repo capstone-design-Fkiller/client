ae0a7f048bb65d60ce0f5465d213147b17b16201
Jnayun <Jnayun@users.noreply.github.com>
2023-05-02 15:35:44 +0000

    Deploy preview for PR 25 🛫

--- pr-preview/pr-25/assets/index-c2c745ee.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as e,a}from"./index-5854cee7.js";import{B as i,P as n}from"./index-424895fe.js";const s=t("section",{target:"e377ic80"})({name:"o0iph5",styles:"display:flex;flex-direction:column;align-items:center;padding:120px 30px 0"}),c=()=>e(n,{children:a(s,{children:[e("div",{children:"페이지 템플릿을 활용해봅니다."}),e(i,{children:"글씨체가 적용이 되나요?"})]})});export{c as default};
