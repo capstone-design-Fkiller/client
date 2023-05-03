da5e6542a665ec4dac4d19a461fd8e66cbb04fdf
jimyu <aliyah_7@naver.com>
2023-05-03 15:07:10 +0900

    feature: 헤더 요소 개별 생성

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -6,39 +6,53 @@ import * as Styled from './style';
 import Icon from '@/components/common/Icon';
 import { IconNames } from '@/components/common/Icon/Icons';
 import { PATH } from '@/utils/path';
-
-interface TopNaviagator {
+interface BoxItem {
   label: string;
   iconName: IconNames;
   path: string;
 }
 
-const topNavigator: TopNaviagator[] = [
-  {
-    label: '로고',
-    iconName: 'home',
-    path: PATH.MAIN,
-  },
-  {
-    label: '마이페이지',
-    iconName: 'user',
-    path: PATH.MAIN,
-  },
-  {
-    label: '쪽지',
-    iconName: 'email',
-    path: PATH.MAIN,
-  },
-];
+interface BoxProps {
+  label: string;
+  items: BoxItem[];
+}
 
 const Header = () => {
+  const logoBox: BoxProps = {
+    label: '로고',
+    items: [
+      {
+        label: 'Logo',
+        //임시로 홈 이모지로 설정
+        iconName: 'home',
+        path: PATH.MAIN,
+      },
+    ],
+  };
+
+  const iconBox: BoxProps = {
+    label: '아이콘 모음',
+    items: [
+      {
+        label: 'Email',
+        iconName: 'email',
+        //나중에 모달이랑 연결 필요
+        path: PATH.NOTICE,
+      },
+      {
+        label: 'Profile',
+        iconName: 'user',
+        //임시로 프로필 버튼 공지 화면으로 연결
+        path: PATH.NOTICE,
+      },
+    ],
+  };
+
   return (
     <Styled.Root>
-      {topNavigator.map(({ label, path, iconName }) => (
-        <Link to={path} key={label} css={Styled.ActiveLink}>
-          <Icon iconName={iconName} size='40' />
-        </Link>
-      ))}
+      <Link to={logoBox.items[0].path} css={Styled.LogoStyle}>
+        <Icon iconName={logoBox.iconName} size='40' />
+      </Link>
     </Styled.Root>
   );
 };
