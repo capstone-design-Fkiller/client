7cdb9d4fc300b32ef1fe1f92011a00329dbe18dc
jimyu <aliyah_7@naver.com>
2023-05-03 21:50:48 +0900

    feature: 버튼 누르면 사용자 전환되도록 재구성

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -7,28 +7,6 @@ import PageTemplate from '@/components/common/PageTamplate';
 
 const MainPage = () => {
   const [userType, setUserTypeState] = useState(false);
-  const UserComponent = () => (
-    <PageTemplate>
-      <Styled.Root>
-        <Styled.LockerInfoConatiner>내 사물함 정보</Styled.LockerInfoConatiner>
-        <Button variant='outlined' css={Styled.ShareButton}>
-          쉐어하기
-        </Button>
-      </Styled.Root>
-    </PageTemplate>
-  );
-  const AdminComponent = () => (
-    <PageTemplate>
-      <Styled.Root>
-        <Button variant='contained' css={Styled.AdminButton}>
-          OO학과 사물함 신청 설정
-        </Button>
-        <Button variant='contained' css={Styled.AdminButton}>
-          OO학과 사물함 배정하기
-        </Button>
-      </Styled.Root>
-    </PageTemplate>
-  );
 
   const MainComponent = () => {
     const handleClick = () => {
@@ -37,38 +15,65 @@ const MainPage = () => {
 
     return (
       <div>
-        <button onClick={handleClick}>관리자/사용자 전환</button>
+        <Button variant='outlined' onClick={handleClick} css={Styled.FlipButton}>
+          관리자/사용자 전환
+        </Button>
         {userType ? <UserComponent /> : <AdminComponent />}
       </div>
     );
   };
 
-  return MainComponent;
-
-  //   if (userType === 'user') {
-  //     return (
-  //       <PageTemplate>
-  //         <Styled.Root>
-  //           <Styled.LockerInfoConatiner>내 사물함 정보</Styled.LockerInfoConatiner>
-  //           <Button variant='outlined' css={Styled.ShareButton}>
-  //             쉐어하기
-  //           </Button>
-  //         </Styled.Root>
-  //       </PageTemplate>
-  //     );
-  //   }
-  //   return (
-  //     <PageTemplate>
-  //       <Styled.Root>
-  //         <Button variant='contained' css={Styled.AdminButton}>
-  //           OO학과 사물함 신청 설정
-  //         </Button>
-  //         <Button variant='contained' css={Styled.AdminButton}>
-  //           OO학과 사물함 배정하기
-  //         </Button>
-  //       </Styled.Root>
-  //     </PageTemplate>
-  //   );
+  return <MainComponent />;
 };
+
+const UserComponent = () => (
+  <PageTemplate>
+    <Styled.Root>
+      <Styled.LockerInfoContainer>내 사물함 정보</Styled.LockerInfoContainer>
+      <Button variant='outlined' css={Styled.ShareButton}>
+        쉐어하기
+      </Button>
+    </Styled.Root>
+  </PageTemplate>
+);
+
+const AdminComponent = () => (
+  <PageTemplate>
+    <Styled.Root>
+      <Button variant='contained' css={Styled.AdminButton}>
+        OO학과 사물함 신청 설정
+      </Button>
+      <Button variant='contained' css={Styled.AdminButton}>
+        OO학과 사물함 배정하기
+      </Button>
+    </Styled.Root>
+  </PageTemplate>
+);
+
+//   if (userType === 'user') {
+//     return (
+//       <PageTemplate>
+//         <Styled.Root>
+//           <Styled.LockerInfoConatiner>내 사물함 정보</Styled.LockerInfoConatiner>
+//           <Button variant='outlined' css={Styled.ShareButton}>
+//             쉐어하기
+//           </Button>
+//         </Styled.Root>
+//       </PageTemplate>
+//     );
+//   }
+//   return (
+//     <PageTemplate>
+//       <Styled.Root>
+//         <Button variant='contained' css={Styled.AdminButton}>
+//           OO학과 사물함 신청 설정
+//         </Button>
+//         <Button variant='contained' css={Styled.AdminButton}>
+//           OO학과 사물함 배정하기
+//         </Button>
+//       </Styled.Root>
+//     </PageTemplate>
+//   );
+// };
 
 export default MainPage;
