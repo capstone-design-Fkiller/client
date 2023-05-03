dc18650122604d1235457dbf5d6f201257bdfc3a
jimyu <aliyah_7@naver.com>
2023-05-03 16:36:10 +0900

    style: 헤더 아이콘 각각 스타일 적용

--- src/components/common/Header/style.tsx
+++ src/components/common/Header/style.tsx
@@ -1,4 +1,4 @@
-import { Theme, css } from '@emotion/react';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Root = styled.header`
@@ -7,21 +7,26 @@ export const Root = styled.header`
   align-items: center;
   gap: 2px;
 
-  width: 400px;
   padding: 10px;
 
   ${({ theme }) =>
     css`
+      width: ${theme.layouts.maxWidth};
       background-color: ${theme.colors.white_300};
       box-shadow: ${theme.shadow.type_3};
     `};
 `;
 
-export const ActiveLink = (theme: Theme) => {
-  return css`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
-  `;
-};
+export const LogoStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const IconStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
