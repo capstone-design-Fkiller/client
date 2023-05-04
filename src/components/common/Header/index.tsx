4d0b7c3ef5ff8b794c2cf4a6a1119b46a17156a1
jimyu <aliyah_7@naver.com>
2023-05-04 16:01:48 +0900

    fix: Box 제거
    - 아이콘 각각 명시

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,68 +1,23 @@
-import { Box } from '@mui/material';
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as Styled from './style';
 
 import Icon from '@/components/common/Icon';
-import { IconNames } from '@/components/common/Icon/Icons';
 import { PATH } from '@/utils/path';
-interface BoxItem {
-  label: string;
-  iconName: IconNames;
-  path: string;
-}
-
-interface BoxProps {
-  label: string;
-  items: BoxItem[];
-}
-
 const Header = () => {
-  const logoBox: BoxProps = {
-    label: '로고',
-    items: [
-      {
-        label: 'Logo',
-        //임시로 홈 이모지로 설정
-        iconName: 'home',
-        path: PATH.MAIN,
-      },
-    ],
-  };
-
-  const iconBox: BoxProps = {
-    label: '아이콘 모음',
-    items: [
-      {
-        label: 'Email',
-        iconName: 'email',
-        //경로 임의 설정
-        path: PATH.NOTICE,
-      },
-      {
-        label: 'Profile',
-        iconName: 'user',
-        //경로 임의 설정
-        path: PATH.APPLY,
-      },
-    ],
-  };
-
   return (
     <Styled.Root>
-      <Box css={Styled.LogoStyle}>
-        {/* 이렇게 말고 PATH.MAIN 이런 식으로 표기하는 게 좋은지 고민 */}
-        <Link to={logoBox.items[0].path}>
-          <Icon iconName={logoBox.items[0].iconName} size='40' />
+      <div>
+        <Link to={PATH.MAIN}>
+          <Icon iconName='home' size='32' />
         </Link>
-      </Box>
+      </div>
       <Styled.IconStyle>
-        <Link to={iconBox.items[0].path}>
-          <Icon iconName={iconBox.items[0].iconName} size='40' />
+        <Link to={PATH.NOTICE}>
+          <Icon iconName='email' size='32' />
         </Link>
-        <Link to={iconBox.items[1].path}>
-          <Icon iconName={iconBox.items[1].iconName} size='40' />
+        <Link to={PATH.APPLY}>
+          <Icon iconName='user' size='32' />
         </Link>
       </Styled.IconStyle>
     </Styled.Root>
