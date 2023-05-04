1011ccabe00fd4a3fe865ed252dcd209c0543303
jimyu <aliyah_7@naver.com>
2023-05-04 16:29:12 +0900

    fix: 불필요한 스타일 제거

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -38,12 +38,8 @@ const UserComponent = () => (
 const AdminComponent = () => (
   <PageTemplate>
     <Styled.Root>
-      <Button variant='contained' css={Styled.UserBtnStyle}>
-        OO학과 사물함 신청 설정
-      </Button>
-      <Button variant='contained' css={Styled.UserBtnStyle}>
-        OO학과 사물함 배정하기
-      </Button>
+      <Button variant='contained'>OO학과 사물함 신청 설정</Button>
+      <Button variant='contained'>OO학과 사물함 배정하기</Button>
     </Styled.Root>
   </PageTemplate>
 );
