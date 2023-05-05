522b5308863d23635253495da8fc3551129d6c1d
정나윤 <02imyun@hufs.ac.kr>
2023-05-06 03:45:36 +0900

    feat: 페이지 번호 컴포넌트 추가

---
+++ src/components/common/Pagination/index.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import Pagination from "@mui/material/Pagination";
+
+const CustomPagination = styled(Pagination)`
+  padding: 20px;
+`;
+
+export default CustomPagination;
