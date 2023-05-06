b3727f3c0457bdec4ec097ab91525f902a645ee6
정나윤 <02imyun@hufs.ac.kr>
2023-05-06 18:24:28 +0900

    del: 페이지 번호 컴포넌트 삭제

--- src/components/common/Pagination/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import Pagination from "@mui/material/Pagination";
-
-const CustomPagination = styled(Pagination)`
-  padding: 20px;
-`;
-
-export default CustomPagination;
