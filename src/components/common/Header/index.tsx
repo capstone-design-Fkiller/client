a687e99211a03b987e1c9533f146d6a34bd4ef7b
jimyu <aliyah_7@naver.com>
2023-05-07 03:03:10 +0900

    feature: 알림 이모지 클릭시 모달 열리게 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Modal } from '@mui/material';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import * as Styled from './style';
@@ -5,22 +7,32 @@ import * as Styled from './style';
 import Icon from '@/components/common/Icon';
 import { PATH } from '@/utils/path';
 const Header = () => {
+  const [alertOpen, setAlertOpen] = React.useState(false);
+
+  const handleAlertOpen = () => {
+    setAlertOpen(true);
+  };
+
   return (
-    <Styled.Root>
-      <div>
-        <Link to={PATH.MAIN}>
-          <Icon iconName='home' size='32' />
-        </Link>
-      </div>
-      <Styled.IconStyle>
-        <Link to={PATH.NOTICE}>
-          <Icon iconName='email' size='32' />
-        </Link>
-        <Link to={PATH.APPLY}>
-          <Icon iconName='user' size='32' />
-        </Link>
-      </Styled.IconStyle>
-    </Styled.Root>
+    <>
+      <Styled.Root>
+        <div>
+          <Link to={PATH.MAIN}>
+            <Icon iconName='home' size='32' />
+          </Link>
+        </div>
+        <Styled.IconStyle>
+          <Icon iconName='email' size='32' onClick={handleAlertOpen} />
+          <Link to={PATH.APPLY}>
+            <Icon iconName='user' size='32' />
+          </Link>
+        </Styled.IconStyle>
+      </Styled.Root>
+
+      <Modal title='알림' open={alertOpen} onClose={() => setAlertOpen(false)}>
+        <h1>알림</h1>
+      </Modal>
+    </>
   );
 };
 
