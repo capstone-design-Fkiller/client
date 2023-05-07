3e1e82840ca2b93b0a1b3345e2cd7276a20cbf3a
seongminn <berry9901@gmail.com>
2023-05-07 18:25:30 +0900

    chore: base url 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-let baseUrl = '/client/';
-if (process.env.PREVIEW_PATH) baseUrl += process.env.PREVIEW_PATH;
+let baseUrl = '/';
+if (process.env.PREVIEW_PATH) baseUrl += `${process.env.PREVIEW_PATH}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
