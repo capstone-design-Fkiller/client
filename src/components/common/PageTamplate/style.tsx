4fb41103a172cdea3839d0819b0a0523b07a1ddd
jimyu <aliyah_7@naver.com>
2023-05-08 04:06:31 +0900

    style: 헤더 알림 모달 스타일 적용
    - 제목 때문에 상단 아이템들이 가려지는 문제 미해결

--- src/components/common/PageTamplate/style.tsx
+++ src/components/common/PageTamplate/style.tsx
@@ -23,9 +23,11 @@ export const Container = styled.div`
     `}
 `;
 
-export const AlertModalContent = styled.div`
-  display: flex;
-  justify-content: center;
+export const AlertModalTitle = styled.div`
+  display: fixed;
+  text-align: center;
+
+  margin-bottom: 15px;
 
   font-size: 18px;
   font-weight: bold;
@@ -33,5 +35,29 @@ export const AlertModalContent = styled.div`
   ${({ theme }) =>
     css`
       color: ${theme.colors.primary_300};
+    `}
+`;
+
+export const ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  height: 90%;
+
+  overflow-x: auto;
+`;
+
+export const AlertModalListItems = styled.div`
+  margin: 3px 3px;
+  padding: 10px;
+
+  font-size: 13px;
+  border-radius: 5px;
+
+  ${({ theme }) =>
+    css`
+      color: ${theme.colors.black};
+      background-color: ${theme.colors.background_2};
     `}
 `;
