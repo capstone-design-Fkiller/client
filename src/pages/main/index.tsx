458a1daf17861db812583bb82127b442b9468da6
jimyu <aliyah_7@naver.com>
2023-05-07 23:59:58 +0900

    chores: 코드리뷰 반영해서 불필요한 컴포넌트 제거

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -8,40 +8,36 @@ import PageTemplate from '@/components/common/PageTamplate';
 const MainPage = () => {
   const [userType, setUserTypeState] = useState(false);
 
-  const MainComponent = () => {
-    const handleClick = () => {
-      setUserTypeState(!userType);
-    };
-
-    return (
-      <div>
-        <Button variant='outlined' onClick={handleClick} css={Styled.FlipButton}>
-          관리자/사용자 전환
-        </Button>
-        {userType ? <UserComponent /> : <AdminComponent />}
-      </div>
-    );
+  const handleClick = () => {
+    setUserTypeState(!userType);
   };
 
-  return <MainComponent />;
+  return (
+    <PageTemplate>
+      <Styled.Root>
+        <div>
+          <Button variant='outlined' onClick={handleClick} css={Styled.FlipButton}>
+            관리자/사용자 전환
+          </Button>
+          {userType ? <UserComponent /> : <AdminComponent />}
+        </div>
+      </Styled.Root>
+    </PageTemplate>
+  );
 };
 
 const UserComponent = () => (
-  <PageTemplate>
-    <Styled.Root>
-      <Styled.LockerInfoContainer>내 사물함 정보</Styled.LockerInfoContainer>
-      <Button variant='outlined'>쉐어하기</Button>
-    </Styled.Root>
-  </PageTemplate>
+  <>
+    <Styled.LockerInfoContainer>내 사물함 정보</Styled.LockerInfoContainer>
+    <Button variant='outlined'>쉐어하기</Button>
+  </>
 );
 
 const AdminComponent = () => (
-  <PageTemplate>
-    <Styled.Root>
-      <Button variant='contained'>OO학과 사물함 신청 설정</Button>
-      <Button variant='contained'>OO학과 사물함 배정하기</Button>
-    </Styled.Root>
-  </PageTemplate>
+  <>
+    <Button variant='contained'>사물함 신청 설정</Button>
+    <Button variant='contained'>사물함 배정하기</Button>
+  </>
 );
 
 export default MainPage;
