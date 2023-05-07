61c8503c9a2b0a47b3f7dc1f8c0ec04218f53a1d
seongminn <berry9901@gmail.com>
2023-05-08 01:12:18 +0900

    refactor: apply page 수정 및 리팩토링

--- src/pages/locker-mode/apply/index.tsx
+++ src/pages/locker-mode/apply/index.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import * as Styled from './style';
 
-import ModalWrapper from '@/components/apply/ModalWrapper';
+import Locker from '@/components/apply/Locker';
 import Button from '@/components/common/Button';
-import Modal from '@/components/common/Modal';
 import PageTemplate from '@/components/common/PageTamplate';
 import useModal from '@/hooks/useModal';
 
@@ -12,43 +11,25 @@ const Structures = ['인문과학관', '교수개발학습원', '국제학사', 
 
 const ApplyPage = () => {
   const [structure, setStructure] = useState<string>('');
-  const [depart, setDepart] = useState<string>('');
   const { open: structureOpen, handleOpen: handleStructureModalOpen } = useModal();
-  const { open: departOpen, handleOpen: handleDepartModalOpen } = useModal();
-  const [lockers, setLockers] = useState<string>();
-
-  useEffect(() => {
-    if (structure && depart) setLockers('사물함');
-  }, [structure, depart]);
+  const [lockers, setLockers] = useState();
 
   return (
     <PageTemplate>
       <Styled.Root>
         {/* // ! structure와 depart를 모두 선택했을 경우 locker 정보를 서버에 요청하고, locker가 load 되었을 경우 정보 렌더링  */}
-        <Styled.LockerInfo>{lockers || '사물함 정보를 선택해주세요.'}</Styled.LockerInfo>
+        {/* // ! 근데 내가 학과 선택 버튼을 만들어뒀는데, 학과는 그냥 사용자 정보에서 빼가는 정보니까 없어도 되지 않나...? */}
 
-        <Styled.InformBox>
-          <Button onClick={handleStructureModalOpen}>{structure || '건물 선택'}</Button>
-          <Button onClick={handleDepartModalOpen}>{depart || '학과 선택'}</Button>
-        </Styled.InformBox>
+        <Styled.Container>
+          {lockers ? <Locker /> : <Locker.Skeleton />}
+          <Styled.InformBox>
+            <span>인문관</span>
+            {/* <hr style={{ width: '10px' }} /> */}
+            <span>스페인어과</span>
+          </Styled.InformBox>
+        </Styled.Container>
+        <Button variant='contained'>신청하기</Button>
       </Styled.Root>
-
-      <Modal title='건물' open={structureOpen} onClose={handleStructureModalOpen}>
-        <ModalWrapper
-          state={structure}
-          setState={setStructure}
-          handleModalOpen={handleStructureModalOpen}
-          list={Structures}
-        />
-      </Modal>
-      <Modal title='학과' open={departOpen} onClose={handleDepartModalOpen}>
-        <ModalWrapper
-          state={depart}
-          setState={setDepart}
-          handleModalOpen={handleDepartModalOpen}
-          list={Structures}
-        />
-      </Modal>
     </PageTemplate>
   );
 };
--- src/pages/locker-mode/apply/style.tsx
+++ src/pages/locker-mode/apply/style.tsx
@@ -13,40 +13,37 @@ export const Root = styled.section`
     `};
 `;
 
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 350px 1fr;
+
+  width: 100%;
+  height: 450px;
+
+  background-color: ${({ theme }) => theme.colors.white_300};
+  border: 1px solid ${({ theme }) => theme.colors.light_grey_100};
+  border-radius: 5px;
+
+  margin-bottom: 30px;
+`;
+
 export const LockerInfo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
   width: 100%;
-  height: 354px;
-
-  border: 1px solid ${({ theme }) => theme.colors.grey_100};
-  border-radius: 5px;
+  height: 100%;
 `;
 
 export const InformBox = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
 
-  margin-top: 50px;
-
-  & button {
-    border: 1px solid ${({ theme }) => theme.colors.grey_100};
-  }
-`;
-
-export const ModalWrapper = styled.section`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
-
-  margin: 30px 0;
-`;
-
-export const ExtendedButton = css`
   width: 100%;
-  aspect-ratio: 1;
-  border: 1px solid;
+  padding: 0 10px;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.light_grey_100};
+  background-color: ${({ theme }) => theme.colors.background_2};
 `;
