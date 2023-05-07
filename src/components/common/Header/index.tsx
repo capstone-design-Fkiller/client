acfe352d97205f1dc6a121d87525d2b25d9a048f
jimyu <aliyah_7@naver.com>
2023-05-08 02:50:09 +0900

    feature: 모달이 전체 화면 위에 뜨도록 위치 변경

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,39 +1,30 @@
-import { Modal } from '@mui/material';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import * as Styled from './style';
 
 import Icon from '@/components/common/Icon';
 import { PATH } from '@/utils/path';
-const Header = () => {
-  const [alertOpen, setAlertOpen] = useState(false);
 
-  const handleAlertOpen = () => {
-    setAlertOpen(!alertOpen);
-  };
+type HeaderProps = {
+  onAlertOpen: () => void;
+};
 
+const Header = ({ onAlertOpen }: HeaderProps) => {
   return (
-    <>
-      <Styled.Root>
-        <Link to={PATH.MAIN}>
-          <Icon iconName='home' size='32' />
+    <Styled.Root>
+      <Link to={PATH.MAIN}>
+        <Icon iconName='home' size='32' />
+      </Link>
+      <Link to={PATH.MAIN} id='header-logo'>
+        HUFS LOCKER
+      </Link>
+      <Styled.HeaderIconsArrange>
+        <Icon iconName='email' size='32' onClick={onAlertOpen} />
+        <Link to={PATH.LOGIN}>
+          <Icon iconName='user' size='32' />
         </Link>
-        <Link to={PATH.MAIN} id='header-logo'>
-          HUFS LOCKER
-        </Link>
-        <Styled.HeaderIconsArrange>
-          <Icon iconName='email' size='32' onClick={handleAlertOpen} />
-          <Link to={PATH.LOGIN}>
-            <Icon iconName='user' size='32' />
-          </Link>
-        </Styled.HeaderIconsArrange>
-      </Styled.Root>
-
-      <Modal title='알림' open={alertOpen} onClose={handleAlertOpen}>
-        <h1>알림</h1>
-      </Modal>
-    </>
+      </Styled.HeaderIconsArrange>
+    </Styled.Root>
   );
 };
 
--- src/components/common/PageTamplate/index.tsx
+++ src/components/common/PageTamplate/index.tsx
@@ -1,20 +1,31 @@
 import { PropsWithChildren } from 'react';
+import { useState } from 'react';
 
 import * as Styled from './style';
 
 import BottomNavigation from '@/components/common/BottomNavigation';
 import Header from '@/components/common/Header';
+import Modal from '@/components/common/Modal';
+import { Style } from '@mui/icons-material';
 
 const PageTemplate = (props: PropsWithChildren) => {
   const { children } = props;
+  const [alertOpen, setAlertOpen] = useState(false);
+
+  const handleAlertOpen = () => {
+    setAlertOpen(!alertOpen);
+  };
 
   return (
     <Styled.Root>
       <Styled.Container>
-        <Header />
+        <Header onAlertOpen={handleAlertOpen} />
         {children}
         <BottomNavigation />
       </Styled.Container>
+      <Modal title='알림' open={alertOpen} onClose={handleAlertOpen}>
+        <Styled.AlertModalContent>알림</Styled.AlertModalContent>
+      </Modal>
     </Styled.Root>
   );
 };
