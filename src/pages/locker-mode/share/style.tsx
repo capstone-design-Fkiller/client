f4574e2e15ffb285a9f51beb54c6461751c36740
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2023-05-09 01:59:48 +0900

    fix: 누락된 스타일 추가

--- src/pages/locker-mode/share/style.tsx
+++ src/pages/locker-mode/share/style.tsx
@@ -20,5 +20,31 @@ export const SelectWrapper = styled.div`
   gap: 10px;
 
   width: 100%;
+  margin: 50px 0 0 0;
+`;
+
+export const DateBox = styled.div`
+  width: 100%;
+  min-height: 46px;
+  padding: 10px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.light_grey_200};
+  border-radius: 5px;
+  outline: none;
+`;
+
+export const DateText = styled.span`
+  font-size: 14px;
+`;
+
+export const DateLabel = styled.span`
+  position: absolute;
+  left: 0;
+  top: -20px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.primary_300};
   margin: 50px 0 30px 0;
 `;
