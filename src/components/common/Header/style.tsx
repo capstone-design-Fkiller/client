8afbd5d93317bb8e0c5d9114e1315fad0ea6aa40
seongminn <berry9901@gmail.com>
2023-05-08 23:54:05 +0900

    style: Logo 스타일 변경

--- src/components/common/Header/style.tsx
+++ src/components/common/Header/style.tsx
@@ -1,31 +1,36 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const Root = styled.header`
+  position: fixed;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 10px;
 
-  height: 55px;
+  max-height: 60px;
 
   ${({ theme }) =>
     css`
       width: ${theme.layouts.maxWidth};
       background-color: ${theme.colors.white_100};
     `};
+`;
 
-  #header-logo {
-    margin-top: 4px;
-    font-size: 24px;
+export const Logo = styled(Link)`
+  font-size: 20px;
+  font-weight: 700;
+  padding-bottom: 5px;
+  border-bottom: 2px dashed ${({ theme }) => theme.colors.primary_300};
 
-    ${({ theme }) =>
-      css`
-        color: ${theme.colors.primary_300};
-      `};
-  }
+  ${({ theme }) =>
+    css`
+      color: ${theme.colors.primary_300};
+    `};
 `;
 
 export const HeaderIconsArrange = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
