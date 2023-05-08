8e37bffdb6df7b054973ae9cbaea5f7ecea23b75
seongminn <berry9901@gmail.com>
2023-05-08 23:17:46 +0900

    fix: baseUrl 추가

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <ThemeProvider theme={emotion_theme}>
           <MuiThemeProvider theme={mui_theme}>
             <Global styles={globalStyle} />
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-let baseUrl = '/';
+let baseUrl = '/client/';
 if (process.env.PREVIEW_PATH) baseUrl += `${process.env.PREVIEW_PATH}`;
 
 // https://vitejs.dev/config/
