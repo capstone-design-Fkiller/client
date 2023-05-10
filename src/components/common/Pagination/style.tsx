7ee3189dfe2562f4abac6b0396fd8196be08d849
정나윤 <02imyun@hufs.ac.kr>
2023-05-10 11:26:49 +0900

    feature: 페이지네이션 컴포넌트 생성

---
+++ src/components/common/Pagination/style.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Pagination = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+interface PageNumberProps {
+  isActive?: boolean;
+  isClickable?: boolean;
+}
+
+export const PageNumber = styled.span<PageNumberProps>`
+  font-size: 1rem;
+  padding: 0.25rem 0.5rem;
+  margin: 0 0.25rem;
+  border-radius: 0.25rem;
+  color: ${(props) => (props.isActive ? 'black' : 'gray')};
+  cursor: ${(props) => (props.isClickable ? 'pointer' : 'default')};
+`;
