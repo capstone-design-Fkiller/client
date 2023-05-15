a496385afba59ca7105752fe96c25b2ebbf8b4d7
seongminn <berry9901@gmail.com>
2023-05-15 19:22:26 +0900

    refactor: user Response data type 및 isLoading 반영, eslint warning 제거

--- src/components/common/Toast/index.tsx
+++ src/components/common/Toast/index.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Alert, Snackbar, Stack } from '@mui/material';
-import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
+import { PropsWithChildren, createContext, useState } from 'react';
 
 type StateOption = 'success' | 'error';
 
--- src/query/user.ts
+++ src/query/user.ts
@@ -1,14 +1,15 @@
+import { useEffect, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 
-import { getLogin, getMe } from '@/api/user';
-import { LoginRequest } from '@/types/user';
+import { getLogin as postLogin, getMe } from '@/api/user';
+import { LoginRequest, UserResponse } from '@/types/user';
 
 const QUERY_KEY = {
   user: 'user',
 };
 
 export const useLogin = () => {
-  const mutation = useMutation((body: LoginRequest) => getLogin(body), {
+  const mutation = useMutation((body: LoginRequest) => postLogin(body), {
     onSuccess: ({ data }) => {
       // ! 임시로 등록해 둔 옵션, 추후 fetchMe api가 개발되면 제거할 예정
       localStorage.setItem('user', JSON.stringify(data.user));
@@ -22,7 +23,13 @@ export const useLogin = () => {
 
 export const useFetchMe = () => {
   // const { data } = useQuery([QUERY_KEY.user], getMe);
-  const data = JSON.parse(localStorage.getItem('user') as string);
+  const [isLoading, setIsLoading] = useState(true);
+  const data: UserResponse = JSON.parse(localStorage.getItem('user') as string);
 
-  return { me: data };
+  useEffect(() => {
+    if (data) setIsLoading(true);
+    else setIsLoading(false);
+  }, [data]);
+
+  return { me: data, isLoading };
 };
