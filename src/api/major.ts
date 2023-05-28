2a0867f9fff5f38e4c2cfdef00ed443c149ac363
jimyu <aliyah_7@naver.com>
2023-05-28 17:42:46 +0900

    feature: type 수정, 기준 설정에 필요한 요소 지정

--- src/api/major.ts
+++ src/api/major.ts
@@ -1,23 +1,39 @@
 import { instance } from './instance';
 
 export interface MajorResponse {
-  apply_end_date: Date | null;
-  apply_start_date: Date | null;
+  apply_end_date: string | null;
+  apply_start_date: string | null;
+  is_baserule_FCFS: boolean;
   id: number;
   name: string;
-  priority_first: string | null;
-  priority_second: string | null;
-  priority_third: string | null;
+  priority_1: string | null;
+  priority_2: string | null;
+  priority_3: string | null;
 }
 
-export type MajorPriorityResponse = Pick<
-  MajorResponse,
-  'priority_first' | 'priority_second' | 'priority_third'
->;
+export type MajorPriorityRequest = Omit<MajorResponse, 'name'>;
+export type MajorPriorityResponse = Pick<MajorResponse, 'priority_1' | 'priority_2' | 'priority_3'>;
+
+export interface RequestPriority extends MajorResponse {
+  user: number;
+  apply_start_date: string | null;
+  apply_end_date: string | null;
+  is_baserule_FCFS: boolean;
+  priority_first?: string | boolean;
+  priority_second?: string | boolean;
+  priority_third?: string | boolean;
+}
 
 export const getMajor = async (params: number) => {
   const url = `/major/${params}`;
   const { data } = await instance.get<MajorResponse>(url);
+
+  return data;
+};
+
+export const putMajor = async (body: Partial<MajorPriorityRequest>) => {
+  const { id, ...args } = body;
+  const { data } = await instance.put<MajorResponse>(`/major/${id}`, args);
 
   return data;
 };
