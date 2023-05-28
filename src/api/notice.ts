5a37a6a45630cf4798d120d6ca1aa45179dfcace
정나윤 <02imyun@hufs.ac.kr>
2023-05-29 04:33:55 +0900

    feature: 공지사항 쿼리 연결

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -2,15 +2,30 @@ import { instance } from './instance';
 
 import { NoticeRequest, NoticeResponse } from '@/types/notice';
 
-export const postNotice = async (notice: NoticeRequest) => {
-  const response = await instance.post<NoticeRequest>('notice/', notice);
+export const postNotice = async (notice: Omit<NoticeRequest, 'id'>) => {
+  const response = await instance.post<NoticeResponse>('notice/', notice);
+
   return response.data;
 };
 
 export const getNotice = async (major: number) => {
   const { data } = await instance.get<NoticeResponse[]>(`notice/?major=${major}`);
-  console.log(data);
+
   return data;
+};
+
+export const getNoticeDetail = async (noticeId: number) => {
+  const { data } = await instance.get<NoticeResponse>(`notice/${noticeId}`);
+
+  return data;
+};
+
+export const putNotice = async (body: NoticeRequest) => {
+  const { id, ...args } = body;
+
+  const response = await instance.put<NoticeResponse>(`notice/${id}`, args);
+
+  return response.data;
 };
 
 export const delNotice = async (id: number) => {
