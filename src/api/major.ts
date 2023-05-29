d556aae4b39a92c300071df37620ea0993f1d210
jimyu <aliyah_7@naver.com>
2023-05-30 00:02:56 +0900

    fix: major api 변경사항 반영

--- src/api/major.ts
+++ src/api/major.ts
@@ -1,29 +1,36 @@
 import { instance } from './instance';
 
 export interface MajorResponse {
+  id: number;
+  priority_1: {
+    name: string | null;
+    question: string | null;
+    is_bool: boolean;
+    is_ascending: boolean;
+  };
+  priority_2: {
+    name: string | null;
+    question: string | null;
+    is_bool: boolean;
+    is_ascending: boolean;
+  };
+  priority_3: {
+    name: string | null;
+    question: string | null;
+    is_bool: boolean;
+    is_ascending: boolean;
+  };
+  name: string;
+  start_date: string | null;
+  end_date: string | null;
   apply_end_date: string | null;
   apply_start_date: string | null;
   is_baserule_FCFS: boolean;
-  id: number;
-  name: string;
-  priority_1: string | null;
-  priority_2: string | null;
-  priority_3: string | null;
 }
 
 // export type MajorPriorityRequest = Omit<MajorResponse, 'name'>;
 export type MajorPriorityRequest = MajorResponse;
 export type MajorPriorityResponse = Pick<MajorResponse, 'priority_1' | 'priority_2' | 'priority_3'>;
-
-export interface RequestPriority extends MajorResponse {
-  user: number;
-  apply_start_date: string | null;
-  apply_end_date: string | null;
-  is_baserule_FCFS: boolean;
-  priority_first?: string | boolean;
-  priority_second?: string | boolean;
-  priority_third?: string | boolean;
-}
 
 export const getMajor = async (params: number) => {
   const { data } = await instance.get<MajorResponse>(`major/${params}`);
