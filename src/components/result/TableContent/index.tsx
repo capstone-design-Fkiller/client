82dd4b140a0fda7eff73190cd87b8fd050a78568
정나윤 <02imyun@hufs.ac.kr>
2023-06-02 00:25:31 +0900

    chore: 필요 없는 파일 제거 및 위치 이동 (2)

---
+++ src/components/result/TableContent/index.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import { Fragment } from 'react';
+
+import { getBuildingName2 } from '@/constants/building';
+import { SortResponse } from '@/types/sort';
+import { YYMMDD } from '@/utils/date';
+
+interface TableContentProps {
+  contents: SortResponse[];
+}
+
+const TableContent = ({ contents }: TableContentProps) => {
+  const renderItem = (content: number | boolean | null) => {
+    if (content === null) {
+      return '-';
+    } else if (typeof content === 'boolean') {
+      return content ? 'O' : 'X';
+    } else {
+      return content;
+    }
+  };
+
+  return (
+    <tbody>
+      {contents.map((result: SortResponse, index: number) => (
+        <Fragment key={result.id}>
+          <Styled.Row>
+            <Styled.Item>{index + 1}</Styled.Item>
+            <Styled.Item>{result.user}</Styled.Item>
+            <Styled.Item>{renderItem(result.priority_1_answer)}</Styled.Item>
+            <Styled.Item>{renderItem(result.priority_2_answer)}</Styled.Item>
+            <Styled.Item>{renderItem(result.priority_3_answer)}</Styled.Item>
+            <Styled.Item>{YYMMDD(result.created_at, true)}</Styled.Item>
+            <Styled.Item>{getBuildingName2(result.building_id)}</Styled.Item>
+          </Styled.Row>
+        </Fragment>
+      ))}
+    </tbody>
+  );
+};
+
+export default TableContent;
+
+const Styled = {
+  Row: styled.tr`
+    font-size: 13px;
+    padding: 10px;
+    width: 100%;
+    transition: 0.1s color ease-in-out;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.light_grey_100};
+    }
+  `,
+
+  Item: styled.td`
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+  `,
+};
