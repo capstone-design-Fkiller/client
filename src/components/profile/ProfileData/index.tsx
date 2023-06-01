474b5d52560dfd518d9b5a89f9f872b2cf21c689
정나윤 <02imyun@hufs.ac.kr>
2023-06-01 16:26:39 +0900

    style: 내 정보 페이지 스타일 재수정

--- src/components/profile/ProfileData/index.tsx
+++ src/components/profile/ProfileData/index.tsx
@@ -18,45 +18,38 @@ const ProfileContainer = (props: ProfileProps) => {
   return (
     <Styled.ProfileContainer>
       <Styled.MyInfo>
-        <div style={{ width: '30%' }}>
-          <Icon iconName='user' size='100' hasCursor={false} />
-        </div>
-        <Styled.InnerBox style={{ width: '70%' }}>
+        <Icon iconName='user' size='100' hasCursor={false} />
+        <Styled.InnerBox>
           <ProfileInfo label='이름' value={me.name} />
           <ProfileInfo label='학과' value={me.major} />
           <ProfileInfo label='학번' value={`${me.id}`} />
           <ProfileInfo label='상태' value={me.is_usermode ? '사용자' : '관리자'} />
         </Styled.InnerBox>
       </Styled.MyInfo>
-
-      {locker ? (
-        <Styled.MyInfo>
-          <div style={{ width: '30%' }}>
+      <Styled.MyInfo className={locker ? 'grid' : 'none'}>
+        {locker ? (
+          <>
             <Icon iconName='locker' size='70' css={Styled.LockerIcon} hasCursor={false} />
-          </div>
-          <Styled.InnerBox style={{ width: '70%' }}>
-            <ProfileInfo label='사물함 번호' value={`${locker.locker_number}번`} />
-            <ProfileInfo
-              label='사물함 위치'
-              value={`${getBuildingName(locker.building_id)} ${locker.floor}층`}
-            />
-            <ProfileInfo
-              label='사용 시작일'
-              value={locker.start_date ? YYMMDD(locker.start_date) : '-'}
-            />
-            <ProfileInfo
-              label='반납 예정일'
-              value={locker.end_date ? YYMMDD(locker.end_date) : '-'}
-            />
-          </Styled.InnerBox>
-        </Styled.MyInfo>
-      ) : (
-        <Styled.MyInfo>
-          <Styled.InfoRow>
-            <p>내 사물함이 없습니다.</p>
-          </Styled.InfoRow>
-        </Styled.MyInfo>
-      )}
+            <Styled.InnerBox>
+              <ProfileInfo label='사물함 번호' value={`${locker.locker_number}번`} />
+              <ProfileInfo
+                label='사물함 위치'
+                value={`${getBuildingName(locker.building_id)} ${locker.floor}층`}
+              />
+              <ProfileInfo
+                label='사용 시작일'
+                value={locker.start_date ? YYMMDD(locker.start_date) : '-'}
+              />
+              <ProfileInfo
+                label='반납 예정일'
+                value={locker.end_date ? YYMMDD(locker.end_date) : '-'}
+              />
+            </Styled.InnerBox>
+          </>
+        ) : (
+          <p>내 사물함이 없습니다.</p>
+        )}
+      </Styled.MyInfo>
     </Styled.ProfileContainer>
   );
 };
--- src/components/profile/ProfileData/style.tsx
+++ src/components/profile/ProfileData/style.tsx
@@ -5,11 +5,13 @@ export const ProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+
+  margin-bottom: 30px;
 `;
 
 export const MyInfo = styled.div`
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: 30% 70%;
   align-items: center;
   padding: 0 15px;
   margin-bottom: 10px;
@@ -17,19 +19,20 @@ export const MyInfo = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.light_grey_200};
   border-radius: 5px;
   height: 150px;
+
+  &.none {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  }
 `;
 
 export const InnerBox = styled.div`
-  flex-direction: column;
-  align-items: center;
-  margin-top: 10px;
-`;
-
-export const InfoRow = styled.div`
   display: flex;
-  align-items: center;
-  padding: 0 15px;
-  margin: 8px 0;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 6px;
 `;
 
 export const LockerIcon = css`
