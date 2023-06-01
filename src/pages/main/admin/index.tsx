646014fc389080b94e3efae7e6ee315965b23327
ChoiHL97 <chlgml9711@naver.com>
2023-06-01 23:57:57 +0900

    배정된 사물함 있으면 배정 버튼 비활성화

--- src/pages/main/admin/index.tsx
+++ src/pages/main/admin/index.tsx
@@ -7,10 +7,13 @@ import Loader from '@/components/common/Loader';
 import PageTemplate from '@/components/common/PageTamplate';
 import { useFetchMe } from '@/query/user';
 import { PATH } from '@/utils/path';
+import { useFetchAssign } from '@/query/assign';
+import { MAJOR } from '@/constants/major';
 
 const AdminMainPage = () => {
   const navigate = useNavigate();
   const { me } = useFetchMe();
+  const { data: assign } = useFetchAssign(MAJOR[me?.major ?? '학과']);
 
   return (
     <PageTemplate>
@@ -20,9 +23,25 @@ const AdminMainPage = () => {
             <Button variant='contained' onClick={() => navigate(PATH.CRITERIA)}>
               사물함 배정기준 설정
             </Button>
-            <Button variant='contained' onClick={() => navigate(PATH.SORT)}>
-              사물함 배정 확정
-            </Button>
+            {assign?.at(0) ?? undefined ? (
+              <>
+                <Button
+                  variant='outlined'
+                  // className={selectedLocker ? '' : 'disabled'}
+                  color='error'
+                  // css={Styled.ExtendedButton}
+                  disableFocusRipple
+                  disableTouchRipple
+                  disabled
+                >
+                  사물함 배정 불가
+                </Button>
+              </>
+            ) : (
+              <Button variant='contained' onClick={() => navigate(PATH.SORT)}>
+                사물함 배정 확정
+              </Button>
+            )}
           </>
         ) : (
           <Loader />
