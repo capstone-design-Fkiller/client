a112ca7c5840d7b132d9b46cffcb7966ac9cd580
seongminn <berry9901@gmail.com>
2023-06-04 01:23:30 +0900

    Updates

--- pr-preview/pr-74/assets/assign-29d51ab0.js
+++
@@ -1,1 +0,0 @@
-import{i as e,f as o,g as r}from"./index-a61a0c3f.js";const i=async s=>{const a=s||0,{data:t}=await e.get(`assign/${a}`);return t},g={assign:"assignResult"},u=s=>{const{createToastMessage:a}=o(),{data:t,isLoading:n}=r([g.assign],()=>i(s),{onError:()=>{a("오류가 발생했습니다.","error")}});return{data:t,isLoading:n}};export{u};
--- pr-preview/pr-74/assets/useInput-4dfd2036.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-a61a0c3f.js";const o=a=>{const[s,e]=t.useState(a),u=t.useCallback(r=>{e(r.target.value)},[]);return{value:s,handleValue:u,setValue:e}};export{o as u};
