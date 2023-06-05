67e5be72b4cfd02f15d074b8baefc05039effe82
seongminn <berry9901@gmail.com>
2023-06-05 16:46:27 +0900

    fix: 예기치 못한 에러 수정 fix: toast 메세지가 undefined일 경우 대비비 fix: apply 시 input 값을 넣지 않았을 때 신청 불가하도록 수정

--- src/components/apply/Condition/index.tsx
+++ src/components/apply/Condition/index.tsx
@@ -3,8 +3,6 @@ import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 
 import Button from '@/components/common/Button';
 import { MajorPriorityAnswerRequest, MajorPriorityResponse, MajorResponse } from '@/types/major';
-import { formatDate } from '@/utils/date';
-import { type } from 'os';
 
 interface ConditionProps {
   majorInfo: MajorResponse | MajorPriorityResponse | undefined;
@@ -46,17 +44,23 @@ const Condition = (props: ConditionProps) => {
         [`${order}_answer`]: value,
       };
 
-      const isAllAnswersFilled = majorConditionList.every(([order]) => {
-        const answer = next[`${order}_answer` as keyof MajorPriorityAnswerRequest];
-        if (typeof answer === 'boolean') {
-          return true;
-        }
-        return answer && answer !== undefined && answer !== null && answer !== '';
-      });
-      setIsSubmitDisabled(!isAllAnswersFilled); // 모든 답변이 입력되었는지에 따라 버튼 활성화 상태 변경
+      handleDisabled(next);
 
       return next;
     });
+  };
+
+  const handleDisabled = (next: Partial<MajorPriorityAnswerRequest>) => {
+    const arr = Object.entries(next).filter(([, value]) => value !== undefined);
+
+    const isAllAnswersFilled = arr.every(([, value]) => {
+      if (value === '-1') return false;
+      if (typeof value !== 'boolean' && !value) return false;
+
+      return true;
+    });
+
+    setIsSubmitDisabled(!isAllAnswersFilled); // 모든 답변이 입력되었는지에 따라 버튼 활성화 상태 변경
   };
 
   return (
