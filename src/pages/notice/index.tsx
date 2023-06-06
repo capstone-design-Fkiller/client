f567585eaf808f37e4d5e2534669bab7ac90195c
seongminn <berry9901@gmail.com>
2023-06-06 21:20:59 +0900

    refactor: notice 페이지 컴포넌트 단위로 분리

--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import * as Styled from './style';
@@ -6,65 +5,27 @@ import * as Styled from './style';
 import Button from '@/components/common/Button';
 import Loader from '@/components/common/Loader';
 import PageTemplate from '@/components/common/PageTamplate';
-import { Pagination } from '@/components/common/Pagination';
-import TableHead from '@/components/common/TableHead';
-import TableContent from '@/components/notice/TableContent';
+import NoticeTable from '@/components/notice/table';
 import { MAJOR } from '@/constants/major';
-import { PAGE_OFFSET } from '@/constants/page_offset';
 import { useFetchNotice } from '@/query/notice';
 import { useFetchMe } from '@/query/user';
 import { PATH } from '@/utils/path';
 
-const TABLE_HEADER = ['ID', '제목', '작성자', '작성일'];
-
 const NoticePage = () => {
   const { me } = useFetchMe();
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [, setSelectedNotice] = useState<number>(0);
   const { data: notices, isLoading } = useFetchNotice(MAJOR[me?.major || 0]);
-  const sortedNotices = notices ? notices.slice() : [];
 
-  const handleNoticeClick = (noticeId: number) => {
-    setSelectedNotice(noticeId);
-    if (noticeId !== 0) {
-      navigate(`${PATH.NOTICE}/${noticeId}`);
-    }
-  };
+  const TABLE_HEADER = ['ID', '제목', '작성자', '작성일'];
 
   return (
     <PageTemplate>
       <Styled.Root>
         <Styled.Title>공지사항</Styled.Title>
         <Styled.TitleMajor>{me?.major}</Styled.TitleMajor>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {notices && notices.length === 0 ? (
-              <Styled.Message>공지사항이 없습니다.</Styled.Message>
-            ) : (
-              <>
-                <Styled.TableContainer>
-                  <TableHead headers={TABLE_HEADER} />
-                  <TableContent
-                    contents={sortedNotices.slice(
-                      (currentPage - 1) * PAGE_OFFSET,
-                      currentPage * PAGE_OFFSET
-                    )}
-                    handleContent={handleNoticeClick}
-                  />
-                </Styled.TableContainer>
-                <Pagination
-                  currentPage={currentPage}
-                  totalItems={sortedNotices.length}
-                  itemsPerPage={PAGE_OFFSET}
-                  setState={setCurrentPage}
-                />
-              </>
-            )}
-          </>
-        )}
+
+        {isLoading ? <Loader /> : <NoticeTable notices={notices} head={TABLE_HEADER} />}
+
         {me?.is_usermode || (
           <Button variant='contained' onClick={() => navigate(PATH.NOTICE_CREATE)}>
             새 공지사항 작성하기
--- src/pages/notice/notice-detail/index.tsx
+++ src/pages/notice/notice-detail/index.tsx
@@ -1,42 +1,29 @@
-import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import * as Styled from './style';
+import * as Styled from '../style';
 
 import Button from '@/components/common/Button';
 import Loader from '@/components/common/Loader';
 import PageTemplate from '@/components/common/PageTamplate';
+import Post from '@/components/notice/Post';
 import { useDeleteNoticeMutation, useFetchNoticeDetail } from '@/query/notice';
 import { useFetchMe } from '@/query/user';
-import { YYMMDD } from '@/utils/date';
 import { PATH } from '@/utils/path';
 
 const NoticeDetailPage = () => {
   const { me } = useFetchMe();
   const navigate = useNavigate();
   const { noticeId } = useParams();
-  const [selectedNotice, setSelectedNotice] = useState(Number(noticeId));
-  const { data: noticeData, isLoading } = useFetchNoticeDetail(selectedNotice);
+  const { data: noticeData, isLoading } = useFetchNoticeDetail(Number(noticeId));
   const { mutate: deleteNoticeMutation } = useDeleteNoticeMutation();
 
-  useEffect(() => {
-    if (!noticeId) {
-      navigate(PATH.NOTICE);
-    } else {
-      const parsedNoticeId = parseInt(noticeId);
-      if (isNaN(parsedNoticeId)) {
-        navigate(PATH.NOTICE);
-      } else {
-        setSelectedNotice(parsedNoticeId);
-      }
-    }
-  }, [noticeId, navigate]);
+  if (isLoading) return <Loader />;
 
   const handleDelete = () => {
-    const res = confirm('정말 삭제하시겠습니까?');
+    const check = confirm('정말 삭제하시겠습니까?');
 
-    if (res) {
-      deleteNoticeMutation(selectedNotice);
+    if (check) {
+      deleteNoticeMutation(Number(noticeId));
       navigate(PATH.NOTICE);
     }
   };
@@ -44,10 +31,6 @@ const NoticeDetailPage = () => {
   const handleEdit = () => {
     navigate(`${PATH.NOTICE}/edit/${noticeId}`);
   };
-
-  if (isLoading || selectedNotice === 0) {
-    return <Loader />;
-  }
 
   if (!noticeData || !noticeId || isNaN(parseInt(noticeId))) {
     return <div>공지사항을 찾을 수 없습니다.</div>;
@@ -58,15 +41,9 @@ const NoticeDetailPage = () => {
       <Styled.Root>
         <Styled.Title>공지사항</Styled.Title>
         <Styled.TitleMajor>{me?.major}</Styled.TitleMajor>
-        <Styled.ContentContainer>
-          <Styled.ContentInfo>
-            공지번호: {noticeData.id} | 공지날짜: {YYMMDD(noticeData.created_at)}
-          </Styled.ContentInfo>
-          <Styled.Content>제목</Styled.Content>
-          <Styled.ContentTitle>{`${noticeData.title}`}</Styled.ContentTitle>
-          <Styled.Content>내용</Styled.Content>
-          <Styled.ContentBody>{noticeData.content}</Styled.ContentBody>
-        </Styled.ContentContainer>
+
+        <Post {...noticeData} created={noticeData.created_at} />
+
         {me?.is_usermode || (
           <Styled.ButtonContainer>
             <Button variant='outlined' onClick={handleEdit}>
@@ -77,6 +54,7 @@ const NoticeDetailPage = () => {
             </Button>
           </Styled.ButtonContainer>
         )}
+
         <Button variant='contained' onClick={() => navigate(PATH.NOTICE)}>
           닫기
         </Button>
