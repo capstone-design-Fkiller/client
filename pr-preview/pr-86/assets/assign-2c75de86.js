592df34452ddf42cc53fcdb5602a48c536ded6c0
son1473 <son1473@users.noreply.github.com>
2023-06-06 08:11:26 +0000

    Deploy preview for PR 86 🛫

--- pr-preview/pr-86/assets/assign-2c75de86.js
+++
@@ -1,1 +0,0 @@
-import{i as e,f as o,g as r}from"./index-62ca51c8.js";const i=async s=>{const a=s||0,{data:t}=await e.get(`assign/${a}`);return t},g={assign:"assignResult"},u=s=>{const{createToastMessage:a}=o(),{data:t,isLoading:n}=r([g.assign],()=>i(s),{onError:()=>{a("오류가 발생했습니다.","error")}});return{data:t,isLoading:n}};export{u};
