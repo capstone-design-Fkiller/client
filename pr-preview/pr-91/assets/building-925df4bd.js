379d6797b5cbf804c3535bb4a5cfdbc93fbaec07
son1473 <son1473@users.noreply.github.com>
2023-06-06 16:00:38 +0000

    Deploy preview for PR 91 🛫

---
+++ pr-preview/pr-91/assets/building-925df4bd.js
@@ -0,0 +1,1 @@
+const n={건물:0,인문과학관:1,교수개발학습원:2,사회과학관:3,국제학사:4,사이버관:5},s={건물:0,인문관:1,교개원:2,사과관:3,국학:4,싸관:5},a=e=>Object.keys(n).find(t=>n[t]===e),c=e=>Object.keys(s).find(t=>s[t]===e);export{n as B,c as a,a as g};
